ca7e2f6f6cbad3e62396cfd1c0cf23e96b39752b
JaneMoroz <moroz.jane@yahoo.com>
2022-07-07 13:37:19 +0300

    Added autobind decorator

--- Drag&Drop Project/src/app.ts
+++ Drag&Drop Project/src/app.ts
@@ -1,3 +1,17 @@
+// Autobind decorator
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+// ProjectInput class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -34,6 +48,7 @@ class ProjectInput {
     this.attach();
   }
 
+  @autobind
   private submitHandler(event: Event) {
     event.preventDefault();
   }
